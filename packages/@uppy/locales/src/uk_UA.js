87a99efb1480b49ad81d13becc90181c969ede16
TheCodeGodOfJava <60669161+TheCodeGodOfJava@users.noreply.github.com>
2020-04-10 21:52:19 +0300

    Create uk_UA.js
    Ukrainian locale

---
+++ packages/@uppy/locales/src/uk_UA.js
@@ -0,0 +1,154 @@
+const ru_RU = {}
+
+ru_RU.strings = {
+  addMoreFiles: 'Додати файли',
+  addingMoreFiles: 'Додавання додаткових файлів',
+  allowAccessDescription: 'Для того щоб зробити фото або відео, будь ласка, дозвольте доступ до вашої камери для цього сайту',
+  allowAccessTitle: 'Будь ласка, дозвольте доступ до вашої камери',
+  authenticateWithTitle: 'Будь ласка, авторизуйтесь в %{pluginName} щоб обрати файли',
+  authenticateWith: 'Підключитись до %{pluginName}',
+  back: 'Назад',
+  addMore: 'Додати ще',
+  browse: 'оберіть',
+  cancel: 'Відмінити',
+  cancelUpload: 'Відмінити завантаження',
+  chooseFiles: 'Обрати файли',
+  closeModal: 'Закрити вікно',
+  companionError: 'Не вдалося підключитися до Companion',
+  complete: 'Готово',
+  connectedToInternet: 'Підключено до інтернета',
+  copyLink: 'Скопіювати посилання',
+  copyLinkToClipboardFallback: 'Скопіюйте посилання',
+  copyLinkToClipboardSuccess: 'Посилання скопійоване до буферу обміну',
+  creatingAssembly: 'Підготовка завантаження...',
+  creatingAssemblyFailed: 'Transloadit: не вдалося створити Assembly',
+  dashboardTitle: 'Завантажувач файлів',
+  dashboardWindowTitle: 'Вікно завантажувача файлів (Натисніть escape, щоб зачинити)',
+  dataUploadedOfTotal: '%{complete} з %{total}',
+  done: 'Готово',
+  dropHereOr: 'Перетягніть файли або %{browse}',
+  dropHint: 'Перетягніть файли сюди',
+  dropPaste: 'Перетягніть файли, вставьте або %{browse}',
+  dropPasteImport: 'Перетягніть файли, вставьте, %{browse} або імпортуйте з:',
+  editFile: 'Редагувати файл',
+  editing: 'Редагуэться %{file}',
+  emptyFolderAdded: 'Файли не були додані — папка пуста',
+  encoding: 'Обробка...',
+  enterCorrectUrl: 'Неправильна адреса: будь ласка, переконайтеся що ви використовуєте пряме посилання на файл',
+  enterUrlToImport: 'Введіть адресу, щоб імпортувати файл',
+  exceedsSize: 'Цей файл більше максимально дозволенного розміру в',
+  failedToFetch: 'Companion не зміг завантажити файл за посиланням, будь ласка, переконайтеся, що адреса вірна',
+  failedToUpload: 'Помалика завантаження %{file}',
+  fileSource: 'Джерело файла: %{name}',
+  filesUploadedOfTotal: {
+    '0': '%{complete} з %{smart_count} файла завантажено',
+    '1': '%{complete} з %{smart_count} файлів завантажено',
+    '2': '%{complete} з %{smart_count} файлів завантажено'
+  },
+  filter: 'Фільтр',
+  finishEditingFile: 'Завершити редагування файла',
+  folderAdded: {
+    '0': 'Додан %{smart_count} файл з %{folder}',
+    '1': 'Додано %{smart_count} файла з %{folder}',
+    '2': 'Додано %{smart_count} файлів з %{folder}'
+  },
+  import: 'Імпортувати',
+  importFrom: 'Імпортувати з %{name}',
+  loading: 'Завантаження...',
+  logOut: 'Вийти',
+  myDevice: 'Мій пристрій',
+  noFilesFound: 'Тут нема файлів або папок',
+  noInternetConnection: 'Нема підключення до інтернету',
+  pause: 'Поставити на паузу',
+  pauseUpload: 'Поставити завантаження на паузу',
+  paused: 'На паузі',
+  preparingUpload: 'Підготовка до завантаження...',
+  processingXFiles: {
+    '0': 'Обробляється %{smart_count} файл',
+    '1': 'Обробляється %{smart_count} файла',
+    '2': 'Обробляється %{smart_count} файлів'
+  },
+  poweredBy: 'Працює на',
+  removeFile: 'Видалити файл',
+  resetFilter: 'Скинути фільтр',
+  resume: 'Продовжити',
+  resumeUpload: 'Продовжити завантаження',
+  retry: 'Півторити спробу',
+  retryUpload: 'Півторити спробу завантаження',
+  saveChanges: 'Зберегти',
+  selectX: {
+    '0': 'Обрати %{smart_count}',
+    '1': 'Обрати %{smart_count}',
+    '2': 'Обрати %{smart_count}'
+  },
+  smile: 'Посміхніться!',
+  startRecording: 'Почався запис відео',
+  stopRecording: 'Завершився запис відео',
+  takePicture: 'Зробити фото',
+  timedOut: 'Завантаження зупинилося на %{seconds} секунд, скасувати',
+  upload: 'Завантажити',
+  uploadComplete: 'Завантаження завершено',
+  uploadFailed: 'Завантаження не вдалося',
+  uploadPaused: 'Завантаження на паузі',
+  uploadXFiles: {
+    '0': 'Завантажити %{smart_count} файл',
+    '1': 'Завантажити %{smart_count} файлів',
+    '2': 'Завантажити %{smart_count} файлів'
+  },
+  uploadXNewFiles: {
+    '0': 'Завантажити +%{smart_count} файл',
+    '1': 'Завантажити +%{smart_count} файлів',
+    '2': 'Завантажити +%{smart_count} файлів'
+  },
+  uploading: 'Завантаження',
+  uploadingXFiles: {
+    '0': 'Завантажується %{smart_count} файл',
+    '1': 'Завантажується %{smart_count} файлів',
+    '2': 'Завантажується %{smart_count} файлів'
+  },
+  xFilesSelected: {
+    '0': '%{smart_count} файл обран',
+    '1': '%{smart_count} файла обрано',
+    '2': '%{smart_count} файлів обрано'
+  },
+  xMoreFilesAdded: {
+    '0': '%{smart_count} додатковий файл додан',
+    '1': '%{smart_count} додаткових файлів додано',
+    '2': '%{smart_count} додаткових файлів додано'
+  },
+  xTimeLeft: 'залишилось %{time}',
+  youCanOnlyUploadFileTypes: 'Ви можете завантажити тільки: %{types}',
+  youCanOnlyUploadX: {
+    '0': 'Ви можете завантажити тільки %{smart_count} файл',
+    '1': 'Ви можете завантажити тільки %{smart_count} файлів',
+    '2': 'Ви можете завантажити тільки %{smart_count} файлів'
+  },
+  youHaveToAtLeastSelectX: {
+    '0': 'Ви повинні обрати хоча б %{smart_count} файл',
+    '1': 'Ви повинні обрати хоча б %{smart_count} файлів',
+    '2': 'Ви повинні обрати хоча б %{smart_count} файлів'
+  },
+  selectAllFilesFromFolderNamed: 'Обрати усі файли з папки %{name}',
+  unselectAllFilesFromFolderNamed: 'Скасувати вибір усіх файлів з папки %{name}',
+  selectFileNamed: 'Обрати файл %{name}',
+  unselectFileNamed: 'Скасувати вибір файла %{name}',
+  openFolderNamed: 'Відкрити папку %{name}'
+}
+
+ru_RU.pluralize = function (n) {
+  if (n % 10 === 1 && n % 100 !== 11) {
+    return 0
+  }
+
+  if (n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20)) {
+    return 1
+  }
+
+  return 2
+}
+
+if (typeof window !== 'undefined' && typeof window.Uppy !== 'undefined') {
+  window.Uppy.locales.uk_UA = uk_UA
+}
+
+module.exports = uk_UA
